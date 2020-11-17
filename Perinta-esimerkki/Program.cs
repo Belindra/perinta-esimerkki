df9c9994414f389c61ecd9de7854f3f33902ee53
Sirene <s2000583@edu.bc.fi>
2020-11-17 13:39:47 +0200

    first commit

---
+++ Perinta-esimerkki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Perinta_esimerkki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
